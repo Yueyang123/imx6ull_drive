6097659e0e5aeb22f45b4800f1ff2c5189b38950
YURI <1700695611@qq.com>
2022-01-07 01:14:50 -0800

    1.7

---
+++ DTSPLATFORM/.leddriver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/yuri/SDK/IMX6ULL/DriverC/DTSPLATFORM/leddriver.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/yuri/SDK/IMX6ULL/DriverC/DTSPLATFORM/leddriver.ko /home/yuri/SDK/IMX6ULL/DriverC/DTSPLATFORM/leddriver.o /home/yuri/SDK/IMX6ULL/DriverC/DTSPLATFORM/leddriver.mod.o
---
+++ DTSPLATFORM/.tmp_versions/leddriver.mod
@@ -0,0 +1,2 @@
+/home/yuri/SDK/IMX6ULL/DriverC/DTSPLATFORM/leddriver.ko
+/home/yuri/SDK/IMX6ULL/DriverC/DTSPLATFORM/leddriver.o
